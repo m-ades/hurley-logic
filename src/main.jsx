8757edb8afd4d284dcfcb9cf8a30c12636e6a3e5
m-ades <m-ades@users.noreply.github.com>
2025-11-18 20:09:59 -0500

    vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './lib/logicpenguin/stylesheets/common.css'
+import './lib/logicpenguin/stylesheets/derivations.css'
+import './lib/logicpenguin/stylesheets/derivation-hardegree.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
